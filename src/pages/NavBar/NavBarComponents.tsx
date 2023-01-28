5321410c79768a0bd0220a83c7ef9f77e3bbdd2d
Olisehemeka Chukwuma <olisehemeka.chukwuma@gmail.com>
2023-01-28 14:08:40 -0700

    nav bar colour

--- src/pages/NavBar/NavBarComponents.tsx
+++ src/pages/NavBar/NavBarComponents.tsx
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 
 export const NavBarContainer = styled.div`
     width: "100%";
-    height: "80px";
+    height: "100px";
     display: flex,;
     justify-content: "space-between";
     padding: "25px 25px";
     position: sticky;
     z-index: 10;
+    background-color: "#000000";
 `;
 
 export const NavBarLogo = styled.div`
